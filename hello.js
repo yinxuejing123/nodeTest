849e2f503b8129ec3cbcc2fc5f43787d1c31794a
yinxuejing123 <follow_myheart1@163.com>
2018-07-26 16:49:59 +0800

    测试Node

--- hello.js
+++ hello.js
@@ -1,6 +1,6 @@
 var http = require('http')
 http.createServer(function (request,response){
     response.writeHead(200,{'Content-Type' : 'text/plain'});
-    response.end('I Miss You Wang')
+    response.end('I hope you have a good time')
 }).listen(8082)
 console.log('Server is running at http://127.0.0.1:8082');
