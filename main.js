4d8dc37c6cc239faed5ace4fc68cbaae2fd53747
yinxuejing123 <follow_myheart1@163.com>
2018-07-27 18:01:07 +0800

    node监听测试

--- main.js
+++ main.js
@@ -31,43 +31,64 @@ var event = require('events');
 
 
 //事件监听的方法合集
-var events = new event.EventEmitter();
-var eventss = require('events').EventEmitter();
-var listener1 = function(){
-    console.log('监听器1')
-}
-var listener2 = function(){
-    console.log('监听器2')
-}
-var listener3 = function(){
-    console.log('监听器3')
-}
-var listener4 = function(){
-    console.log('监听器4')
-}
-//增加监听器
-events.addListener('connection',listener1);
-//绑定监听器
-events.on('connection',listener2);
-//增加监听器
-events.addListener('connection',listener3);
+// var events = new event.EventEmitter();
+// var eventss = require('events').EventEmitter();
+// var listener1 = function(){
+//     console.log('监听器1')
+// }
+// var listener2 = function(){
+//     console.log('监听器2')
+// }
+// var listener3 = function(){
+//     console.log('监听器3')
+// }
+// var listener4 = function(){
+//     console.log('监听器4')
+// }
+// //增加监听器
+// events.addListener('connection',listener1);
+// //绑定监听器
+// events.on('connection',listener2);
+// //增加监听器
+// events.addListener('connection',listener3);
+//
+// //多个监听器执行
+// var eventsList = require('events').EventEmitter.listenerCount(events,'connection');
+// console.log('共有'+eventsList+'监听器在执行')
+//
+// events.emit('connection');
+//
+// //移除监听器3
+// events.removeListener('connection',listener3);
+// console.log('不再监听监听器3');
+//
+// //增加监听器
+// events.addListener('connection',listener4);
+//
+// events.emit('connection');
+//
+// eventsList = require('events').EventEmitter.listenerCount(events,'connection');
+// console.log("还剩下"+eventsList+"个监听器");
+//
+// console.log("程序运行结束")
 
-//多个监听器执行
-var eventsList = require('events').EventEmitter.listenerCount(events,'connection');
-console.log('共有'+eventsList+'监听器在执行')
 
-events.emit('connection');
+//缓冲区实例
+// buf = Buffer.alloc(26);
+// for(var i=0;i<26;i++){
+//     buf[i] = i + 97
+// }
+// console.log(buf.toString('ascii'));
+// console.log(buf.toString('ascii',0,5));
+// console.log(buf.toString('utf8',0,5));
+// console.log(buf.toString(undefined,0,5));
 
-//移除监听器3
-events.removeListener('connection',listener3);
-console.log('不再监听监听器3');
 
-//增加监听器
-events.addListener('connection',listener4);
+const buf = Buffer.from([0x1,0x2,0x3,0x4,0x5,0x6,0x7,0x8,0x9,0x10,0x11,0x12,0x13,0x14,0x15,0x16]);
+const json = JSON.stringify(buf);
+console.log(json)
 
-events.emit('connection');
-
-eventsList = require('events').EventEmitter.listenerCount(events,'connection');
-console.log("还剩下"+eventsList+"个监听器");
-
-console.log("程序运行结束")
+const copy = JSON.parse(json,(key,value)=>{
+    return value && value.type === 'Buffer'?Buffer.from(value.data):value;
+});
+console.log(copy);
